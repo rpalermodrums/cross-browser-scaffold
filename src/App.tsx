c5ba345379dbee92a59925c1b84aec1f057d1eaf
Ryan  Palermo <rpalermodrums@gmail.com>
2023-01-02 16:33:24 -0500

    add new event page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { Button, Text, View } from 'react-native';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  useNavigate,
+} from 'react-router-dom';
+
+import NewEvent from './components/create/NewEvent';
+
+function Home () {
+  const navigate = useNavigate();
+
+  return (
+    <View>
+      <Text>
+        Home
+      </Text>
+      <Button
+        onPress={() => navigate('/new-event')}
+        title='New Event'
+        accessibilityLabel='New Event'
+        testID='new-event-button'
+      />
+    </View>
+  )
+}
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/'>
+      <Route index element={<Home />} />
+      <Route path='new-event' element={<NewEvent />} />
+    </Route>,
+  ),
+);
 
 function App () {
   return (
     <View>
-      <Text>
-        Hello World
-      </Text>
+      <RouterProvider router={router} />
     </View>
   );
 }
---
+++ src/components/create/NewEvent.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function NewEvent () {
+  console.log('New Event');
+  return (
+    <View>
+      <Text>
+        New Event
+      </Text>
+    </View>
+  );
+}
+
+export default NewEvent;
+
